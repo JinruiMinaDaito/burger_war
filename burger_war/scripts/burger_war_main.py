38b95032a6496f9007748edd00eb4a4d874cbfa6
YoshidaOyabun <61648534+YoshidaOyabun@users.noreply.github.com>
2020-03-18 08:11:51 +0900

    Add files via upload

--- burger_war/scripts/burger_war_main.py
+++ burger_war/scripts/burger_war_main.py
@@ -12,11 +12,12 @@ import smach_ros
 import roslib.packages
 from geometry_msgs.msg    import Twist
 from std_msgs.msg         import Float32
+from move_base_msgs.msg import MoveBaseGoal
 
 rospy.init_node("burger_war_main_node")#smath_filesでtfを使用するため,init_nodeする前にtf_listerner()があるとエラー
 from smach_files          import *
 
-# Global変数
+# Global変数west_right
 target_location_global = ""
 
 
@@ -25,7 +26,7 @@ class Commander(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=["move", "fight", "commander", "game_finish"])
 
-        self.check_points     = ["south_center", "south_left", "south_right", "west_center", "west_left", "west_right", "north_center", "north_left", "north_right", "east_center", "east_left", "east_right"]
+        self.check_points     = ["south_center", "south_left", "south_right", "west_right", "west_center", "west_left", "east_left", "east_center", "east_right", "north_center", "north_left", "north_right"]
         self.last_notice_time = rospy.Time.now()
         self.is_enemy_close   = False
 
@@ -37,7 +38,7 @@ class Commander(smach.State):
         rospy.sleep(1)
 
     def execute(self, userdata):
-        global target_location_global
+        global target_location_global"east_right"
 
         try:
             target_frame_name = rospy.get_param('~robot_name',"") + '/enemy_closest'
@@ -50,8 +51,8 @@ class Commander(smach.State):
             self.is_enemy_close = False
 
         #各状況に合わせて状態遷移
-        #if self.is_enemy_close == True:
-        #    return "fight"
+        if self.is_enemy_close == True:
+            return "fight"
         if len(self.check_points) > 0:
             target_location_global = self.check_points[0]
             self.check_points.pop(0)
@@ -68,7 +69,7 @@ class Commander(smach.State):
             move_base.cancel_goal()
             self.last_notice_time = rospy.Time.now() #最後に敵を確認した時刻
             self.is_enemy_close   = True
-            self.check_points.reverse() #敵がいた場合は、逆方向の残りのチェックポイントを取りに行く
+
         elif msg.data > notice_length and self.is_enemy_close == True:
             self.is_enemy_close == False
 
@@ -86,22 +87,17 @@ class Move(smach.State):
 
         #移動開始
         result = move_base.send_goal_and_wait_result(goal)
-
+        rospy.loginfo(goal)
         if result == True:
             rospy.loginfo("Turtlebot reached at [" + target_location_global + "].")
             overlaytext.publish("Turtlebot reached at [" + target_location_global + "].")
-            #退避動作開始
-            send_data = Twist()
-            send_data.linear.x = -10
-            self.pub_twist.publish(send_data)
-            rospy.sleep(2)
         else:
             rospy.loginfo("Moving Failed [" + target_location_global + "].")
             overlaytext.publish("Moving Failed [" + target_location_global + "].")
 
         return "finish"
 
-#本番では敵の位置をゴールに設定し、取りに行く
+#敵の位置をゴールに設定し、取りに行く
 class Fight(smach.State):
 
     def __init__(self):
@@ -112,23 +108,34 @@ class Fight(smach.State):
     def execute(self, userdata):
 
         overlaytext.publish("STATE: Fight")
+        goal = MoveBaseGoal()
 
         try:
             target_frame_name = rospy.get_param('~robot_name',"") + '/enemy_closest'
-            source_frame_name = rospy.get_param('~robot_name',"") + "/base_footprint"
-            goal = self.tf_listener.lookupTransform(source_frame_name, target_frame_name, rospy.Time(0))
+            source_frame_name = "/map"
+            enemy_pos = self.tf_listener.lookupTransform(source_frame_name, target_frame_name, rospy.Time(0))
 
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.logerr("TF lookup error [base_footprint -> enemy_closest]")
             rospy.sleep(1)
             return "finish"
 
+        #目標位置の設定
+        goal.target_pose.header.stamp    = rospy.Time.now()
+        goal.target_pose.header.frame_id = rospy.get_param('~robot_name', '') + "/map"
+        goal.target_pose.pose.position.x    = enemy_pos[0][0]
+        goal.target_pose.pose.position.y    = enemy_pos[0][1]
+        goal.target_pose.pose.position.z    = enemy_pos[0][2]
+        goal.target_pose.pose.orientation.x = enemy_pos[1][0]
+        goal.target_pose.pose.orientation.y = enemy_pos[1][1]
+        goal.target_pose.pose.orientation.z = enemy_pos[1][2]
+        goal.target_pose.pose.orientation.w = enemy_pos[1][3]
+
         #移動開始
         overlaytext.publish("Move to enemy position")
         rospy.loginfo("move to eney position")
         rospy.loginfo(goal)
-        result = move_base.send_goal_and_wait_result(goal)
-        rospy.sleep(1)
+        result = move_base.send_goal(goal)
 
         return "finish"
 
